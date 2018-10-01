6eaf06cf593e1aeec70497cdf48e95e556136336
Ralphy <ralphy.lee@qq.com>
2018-10-01 22:10:54 +0800

    切换场景流程

--- RoyalWar/Assets/Assets/Scripts/Procedure/ProcedureChangeScene.cs
+++ RoyalWar/Assets/Assets/Scripts/Procedure/ProcedureChangeScene.cs
@@ -1,15 +1,108 @@
 ﻿using System;
+using GameFramework;
+using GameFramework.DataTable;
+using GameFramework.Event;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
 
 namespace RoyalWar
 {
     public class ProcedureChangeScene : ProcedureBase
     {
+        private const int MenuSceneId = 1;
+
+        private bool m_ChangeToMenu = false;
+        private bool m_IsChangeSceneComplete = false;
+        private int m_BackgroundMusicId = 0;
+
         public override bool UseNativeDialog
         {
             get
             {
-                throw new NotImplementedException();
+                return false;
             }
+        }
+
+        protected override void OnEnter(ProcedureOwner procedureOwner)
+        {
+            Log.Debug("进入ProcedureChangeScene流程");
+            base.OnEnter(procedureOwner);
+
+            m_IsChangeSceneComplete = false;
+
+            GameEntry.Event.Subscribe(LoadSceneSuccessEventArgs.EventId, OnLoadSceneSuccess);
+            GameEntry.Event.Subscribe(LoadSceneFailureEventArgs.EventId, OnLoadSceneFailure);
+            GameEntry.Event.Subscribe(LoadSceneUpdateEventArgs.EventId, OnLoadSceneDependencyAsset);
+            GameEntry.Event.Subscribe(LoadSceneDependencyAssetEventArgs.EventId, OnLoadSceneDependencyAsset);
+
+            //隐藏所有实体
+            GameEntry.Entity.HideAllLoadingEntities();
+            GameEntry.Entity.HideAllLoadedEntities();
+
+            //卸载所有场景
+            string[] loadedSceneAssetNames = GameEntry.Scene.GetLoadedSceneAssetNames();
+            for(int i = 0; i < loadedSceneAssetNames.Length; i++)
+            {
+                GameEntry.Scene.UnloadScene(loadedSceneAssetNames[i]);
+            }
+
+            //还原游戏速度
+            GameEntry.Base.ResetNormalGameSpeed();
+
+            int sceneId = procedureOwner.GetData<VarInt>(Constant.ProcedureData.NextSceneId).Value;
+            m_ChangeToMenu = (sceneId == MenuSceneId);
+            IDataTable<DRScene> dtScene = GameEntry.DataTable.GetDataTable<DRScene>();
+
+            DRScene drScene = dtScene.GetDataRow(sceneId);
+
+            if(drScene == null)
+            {
+                Log.Warning("Can not load scene '{0}' from data table.", sceneId.ToString());
+                return;
+            }
+
+            GameEntry.Scene.LoadScene(AssetUtility.GetSceneAsset(drScene.AssetName), Constant.AssetPriority.SceneAsset, this);
+            m_BackgroundMusicId = drScene.BackgroundMusicId;
+        }
+
+        protected override void OnLeave(ProcedureOwner procedureOwner,bool isShutdown)
+        {
+            GameEntry.Event.Unsubscribe(LoadSceneSuccessEventArgs.EventId, OnLoadSceneSuccess);
+            GameEntry.Event.Unsubscribe(LoadSceneFailureEventArgs.EventId, OnLoadSceneFailure);
+            GameEntry.Event.Unsubscribe(LoadSceneUpdateEventArgs.EventId, OnLoadSceneDependencyAsset);
+            GameEntry.Event.Unsubscribe(LoadSceneDependencyAssetEventArgs.EventId, OnLoadSceneDependencyAsset);
+            base.OnLeave(procedureOwner, isShutdown);
+        }
+
+        protected override void OnUpdate(ProcedureOwner procedureOwner,float elapseSeconds,float realElapseSeconds)
+        {
+            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+            if (!m_IsChangeSceneComplete)
+            {
+                return;
+            }
+            if (m_ChangeToMenu)
+            {
+                ChangeState<ProcedureMenu>(procedureOwner);
+            }else
+            {
+                ChangeState<ProcedureMain>(procedureOwner);
+            }
+        }
+
+        private void OnLoadSceneDependencyAsset(object sender, GameEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnLoadSceneFailure(object sender, GameEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnLoadSceneSuccess(object sender, GameEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
